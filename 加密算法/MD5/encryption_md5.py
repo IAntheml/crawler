7e1c3596ed680c2f88dd48c284ef300f5d595d06
Administrator <393089025@qq.com>
2023-04-24 17:39:48 +0800

    加密算法初始化

--- 加密算法/MD5/encryption_md5.py
+++ 加密算法/MD5/encryption_md5.py
@@ -1,3 +1,13 @@
-import requests
+# // 在依赖项中添加包： --save
+# npm install crypto-js  --save
 
-print("test")
+# 使用案例
+import hashlib
+
+def md5_test():
+    md5 = hashlib.md5()
+    md5.update('python'.encode('utf-8'))
+    print(md5.hexdigest())
+
+if __name__ == '__main__':
+    md5_test()
---
+++ 加密算法/MD5/js_md5.js
@@ -0,0 +1,8 @@
+var CryptoJS = require('crypto-js')
+
+function MD5Test() {
+    var text = "I love python!"
+    return CryptoJS.MD5(text).toString()
+}
+
+console.log(MD5Test())
