7ed1c86d85bcde71413bc262e7dc3a135633b5fe
Lian <393089025@qq.com>
2023-04-27 23:56:33 +0800

    des相关

--- 加密算法/对称加密/encryption_des.py
+++ 加密算法/对称加密/encryption_des.py
@@ -4,8 +4,8 @@ DES: 56位密钥，由于密钥太短，被逐渐被弃用。
 AES: 有128位、192位、256位密钥，现在比较流行。密钥长、可以增加破解的难度和成本。
 
 工作模式归纳
-1. ECB模式：全称Electronic Codebook模式，译为电子密码本模式，每个数据块独立进行加/解密
-2. CBC模式：全称Cipher Block Chaining模式，译为密文分组链接模式
+1. ECB模式：全称Electronic Codebook模式，译为电子密码本模式，每个数据块独立进行加/解密 （有iv初始向量）
+2. CBC模式：全称Cipher Block Chaining模式，译为密文分组链接模式（无iv初始向量）
 3. CFB模式：全称Cipher FeedBack模式，译为密文反馈模式
 4. OFB模式：全称Output Feedback模式，译为输出反馈模式。
 5. CTR模式：全称Counter模式，译为计数器模式。
