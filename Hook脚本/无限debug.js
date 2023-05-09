46a7dda5b89dc968a0ecf094d3f6d1a138ad0c7a
Administrator <393089025@qq.com>
2023-05-09 10:15:09 +0800

    加密算法初始化

--- Hook脚本/无限debug.js
+++ Hook脚本/无限debug.js
@@ -1,3 +1,5 @@
+// 方式一
+
 Function.prototype.__constructor_back = Function.prototype.constructor;
 Function.prototype.constructor = function() {
     if(arguments && typeof arguments[0]==='string'){
@@ -6,4 +8,19 @@ Function.prototype.constructor = function() {
         }
     }
    return Function.prototype.__constructor_back.apply(this,arguments);
+}
+
+
+
+
+// 方式二
+
+
+var _constructor = constructor;
+Function.prototype.constructor = function(s) {
+    if (s == "debugger") {
+        console.log(s);
+        return null;
+    }
+    return _constructor(s);
 }
