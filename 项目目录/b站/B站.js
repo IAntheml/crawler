b30ad27f637b38be573157f8bc5116fc9beba817
Administrator <393089025@qq.com>
2023-05-18 21:16:24 +0800

    B站APP

--- 项目目录/b站/B站.js
+++ 项目目录/b站/B站.js
@@ -1,3 +1,23 @@
 /*
     软件版本可以在豌豆荚上下载V6.24.0
  */
+
+
+/*
+反编译：用jadx打开apk后点击放大镜建立索引
+ */
+
+/*
+搜索定位请求的方式：请求体、请求头、url，hook常见加密算法，hook treeMap或者HashMap的put方法
+ */
+
+/*
+字节数组输出成object时，可以调用序列化方法JSON.stringify(res)将结果转化为字节字符串输出
+
+注意：在表示字节时java是有符号的，python是无符号的，需要转换。
+ */
+
+
+/*
+常用几个id的python实现：AndroidID、UUID、mac、IMEI
+ */
