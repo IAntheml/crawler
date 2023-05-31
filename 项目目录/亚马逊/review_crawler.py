a18434d925d4a2ef0d1828f46e3fb53f924802a6
Lian <393089025@qq.com>
2023-06-01 00:10:55 +0800

    亚马逊

--- 项目目录/亚马逊/review_crawler.py
+++ 项目目录/亚马逊/review_crawler.py
@@ -1,4 +1,10 @@
 import requests
+import re
+
+
+def get_hit():
+    pass
+
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
@@ -24,7 +30,41 @@ session_id = cookies.get("session-id")
 session_id_time = cookies.get("session-id-time")
 i_pref = cookies.get("i18n-prefs")
 sp_cdn = cookies.get("sp-cdn")
+
+headers_sec = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
+    "Sec-Ch-Ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+}
+cookies_sec = {
+    "session-id": session_id,
+    "session-id-time": session_id_time,
+    "i18n-prefs": i_pref,
+    "sp-cdn": sp_cdn
+}
+
+response = requests.get(url, headers=headers_sec, cookies=cookies_sec)
+
+ubid_main = response.cookies.get("ubid-main")
+ue_id = re.findall("ar ue_id = '(.*?)',", response.text)
+
+csm_hit = get_hit(ue_id)
+
+print(response.status_code)
 print(response.text)
+
+response.text
 
 # cookies = {
 #     "session-id": session_id,
