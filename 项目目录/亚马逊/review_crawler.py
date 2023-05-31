b8a803aaacab42f98784606444493ecea83c38dc
Administrator <393089025@qq.com>
2023-05-31 18:00:45 +0800

    amazon项目建表

--- 项目目录/亚马逊/review_crawler.py
+++ 项目目录/亚马逊/review_crawler.py
@@ -1,43 +1,41 @@
 import requests
 
-
 headers = {
-    "authority": "www.amazon.com",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "no-cache",
-    "device-memory": "8",
-    "downlink": "1.35",
-    "dpr": "1",
-    "ect": "3g",
-    "pragma": "no-cache",
-    "rtt": "400",
-    "sec-ch-device-memory": "8",
-    "sec-ch-dpr": "1",
-    "sec-ch-ua": "^\\^Google",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "^\\^Windows^^",
-    "sec-ch-ua-platform-version": "^\\^15.0.0^^",
-    "sec-ch-viewport-width": "719",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-    "viewport-width": "719"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
+    "Sec-Ch-Ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 }
-cookies = {
-    "session-id": "142-5745479-3517763",
-    "session-id-time": "2082787201l",
-    "i18n-prefs": "USD",
-    "ubid-main": "130-6664348-6276527",
-    "session-token": "^\\^uO7/dRhivyi9UmKpC9qdQ4pYw2/vHv/QLdbQ/ncc/uTgWlahjNCYuWP/0PpRGi6VfZOXxt11cHBTXjRWwKGDLd/56sSE6yakhI42/lGv76BtovCvPNTYsMzUdvzY2yDAxpbt6DApWY8BWSAyTs65H4NzFwGxBP79RBuEyYT6LHb2dZBbZgiL5i9J1QhJX9JUXszbvBvpzIPDXUss0ckOeJSCAx8CVtk8u66QeyvMu0A=^^",
-    "csm-hit": "tb:K7ARK7H4T2HF7MDFNCBY+s-K7ARK7H4T2HF7MDFNCBY^|1684490008875&t:1684490008875&adb:adblk_no"
-}
-url = "https://www.amazon.com/TP-Link-Deco-Whole-System-2-Pack/product-reviews/B0797D6853"
-response = requests.get(url, headers=headers, cookies=cookies)
+url = "https://www.amazon.com/gp/bestsellers/pc/17935294011"
+response = requests.get(url, headers=headers)
+cookies = response.cookies
 
-print(response.status_code)
+session_id = cookies.get("session-id")
+session_id_time = cookies.get("session-id-time")
+i_pref = cookies.get("i18n-prefs")
+sp_cdn = cookies.get("sp-cdn")
 print(response.text)
-print(response)
+
+# cookies = {
+#     "session-id": session_id,
+#     "session-id-time": session_id_time,
+#     "i18n-prefs": i_pref,
+#     "sp-cdn": sp_cdn,
+#     # "csm-hit": "tb:8C0PV9WB478MEQC0WNAB+s-H351ND075PREQNFMT1KK^|1681876800436&t:1681876800436&adb:adblk_no",
+#     # "ubid-main": "134-7327429-3111024",
+#     # "session-token": "o/pAGdEPnR3oI+H39EMrXiMV/IBNOet9oEAIzWOg5Gh7NLtcE7D97axwCXUO4LOSxi2oTcjLjfugdMfzEz2TEIQ/n7ZLj+ghaVq1NLsyID8SmmRz7gW8tx+fye0OdbXRaEa96pWIz2z27nGQ5XtuR2oJhDiwKGYISpEZ3CBXA1QbcaEnidVMgwBts+IGrifI7LGpM2HXLMQIZE3lul+2pv9R+j7RaK2pGWnZ32YME1U="
+# }
+#
+# response = requests.get(url, headers=headers,cookies = cookies)
+# print(response.status_code)
+# print(response.text)
