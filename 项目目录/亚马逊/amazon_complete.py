cfb3ef174187cffab06133ddfdf3dd1fc0f16103
Lian <393089025@qq.com>
2023-06-03 00:55:32 +0800

    项目目录

--- 项目目录/亚马逊/amazon_complete.py
+++ 项目目录/亚马逊/amazon_complete.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import time
 
@@ -8,7 +9,7 @@ import re
 """
 类目链接，作为单独一次request请求去拿
 """
-headers_category = {
+"""headers_category = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
@@ -51,7 +52,7 @@ for catalog in catalog_list:
     catalog_url = "https://www.amazon.com/" + catalog
     catalog_url_list.append(catalog_url)
 
-# print(catalog_url_list)
+# print(catalog_url_list)"""
 
 """
 类目链接存放数据库后，拿到类目链接，请求首页
@@ -59,30 +60,38 @@ for catalog in catalog_list:
 
 
 def get_cookie(response, cookie):
-    if response.status_code == 429:
-        cookies = response.cookies
+    cookies = response.cookies
+    if cookies:
         session_id = cookies.get("session-id")
         session_id_time = cookies.get("session-id-time")
         i_pref = cookies.get("i18n-prefs")
         sp_cdn = cookies.get("sp-cdn")
-        cookie["session-id"] = session_id
-        cookie["session-id-time"] = session_id_time
-        cookie["i18n-prefs"] = i_pref
-        cookie["sp-cdn"] = sp_cdn
-        return cookie
-    elif response.status_code == 200:
-        ubid_main = response.cookies.get("ubid-main")
-        if ubid_main:
-            cookie["ubid-main"] = ubid_main
-        ue_id = re.findall("ar ue_id = '(.*?)',", response.text)[0]
-        if ue_id:
-            cookie["csm-hit"] = "tb:s-" + ue_id + "|" + str(int(time.time()*1000)) + '&t:' + str(int(time.time()*1000)) + "&adb:adblk_no"
-        return cookie
+        if session_id:
+            cookie["session-id"] = session_id
+        if session_id_time:
+            cookie["session-id-time"] = session_id_time
+        if i_pref:
+            cookie["i18n-prefs"] = i_pref
+        if sp_cdn:
+            cookie["sp-cdn"] = sp_cdn
+        if response.status_code == 429:
+            return cookie
+        elif response.status_code == 200:
+            ubid_main = cookies.get("ubid-main")
+            if ubid_main:
+                cookie["ubid-main"] = ubid_main
+            ue_id = re.findall("ar ue_id = '(.*?)',", response.text)[0]
+            if ue_id:
+                cookie["csm-hit"] = "tb:s-" + ue_id + "|" + str(int(time.time() * 1000)) + '&t:' + str(
+                    int(time.time() * 1000)) + "&adb:adblk_no"
+            return cookie
+
 
 def get_token(response):
     if response.status_code == 200:
         token = re.findall('data-acp-params="(.*?)"', response.text)[0]
         return token
+
 
 def parse_rank_first(response):
     first_page_item = []
@@ -124,6 +133,7 @@ def parse_rank_first(response):
         first_page_item.append(item)
     return first_page_item
 
+
 def get_next_page_url(html):
     next_page = ""
     page_url = html.xpath('//li[@class="a-last"]/a/@href')
@@ -132,33 +142,71 @@ def get_next_page_url(html):
     return next_page
 
 
-def get_scroll_page_data(scroll_page,bs_category):
+def get_scroll_page_data(index, scroll_page, bs_category):
+    scroll_page = json.loads(str(scroll_page))
     id_list = []
     index_list = []
-    bs_category = "zg_bs_300189"
-
     for item in scroll_page:
-        index_list.append(int(item["metadataMap"]["render.zg.rank"]) -1)
-        id_item = {}
-        id_item["id"] = item["id"]
-
-    offset = 0
-    if len(index_list)>0:
-        offset = index_list[0]
-
+        if int(item["metadataMap"]["render.zg.rank"]) > index:
+            index_list.append(int(item["metadataMap"]["render.zg.rank"]) - 1)
+            # 注意字典顺序，json序列化为字符串后是有顺序的！
+            id_item = {"id": item["id"], "metadataMap": item["metadataMap"], "linkParameters": {}}
+            id_item = json.dumps(id_item,separators=(',', ':'))
+            id_list.append(id_item)
+    # offset = 0
+    # if len(index_list) > 0:
+    #     offset = index_list[0]
     data = {
         "faceoutkataname": "GeneralFaceout",
         "ids": id_list,
-        "indexes" : index_list,
+        "indexes": index_list,
         "linkparameters": "",
-        "offset": offset,
+        "offset": index,
         "reftagprefix": bs_category
     }
     return data
 
 
-def get_scroll_page_item(data, cookies, token):
-    return []
+def get_scroll_page_item(data, cookies, token, referer):
+    headers_scroll = {
+        "Accept": "text/html, application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json",
+        "Device-Memory": "8",
+        "Downlink": "10",
+        "Dpr": "1",
+        "Ect": "4g",
+        "Origin": "https://www.amazon.com",
+        "Pragma": "no-cache",
+        "Referer": referer,
+        "Rtt": "200",
+        "Sec-Ch-Device-Memory": "8",
+        "Sec-Ch-Dpr": "1",
+        "Sec-Ch-Ua": "\"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
+        "Sec-Ch-Ua-Platform-Version": "\"15.0.0\"",
+        "Sec-Ch-Viewport-Width": "1135",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "Viewport-Width": "1135",
+        "X-Amz-Acp-Params": token,
+        "X-Requested-With": "XMLHttpRequest"
+    }
+
+    url_scroll = "https://www.amazon.com/acp/p13n-zg-list-grid-desktop/p13n-zg-list-grid-desktop-66060a50-59dd-41d8-a7cf-383304720513-1679651361432/nextPage"
+    params_scroll = {
+        "page-type": "undefined",
+        "stamp": str(int(time.time() * 1000))
+    }
+    response_scroll = requests.post(url=url_scroll, headers=headers_scroll, params=params_scroll, cookies=cookies,
+                                    json=data)
+    print(response_scroll.status_code)
+    print(response_scroll.text)
 
 
 def get_page(url, page_type, cookies):
@@ -194,10 +242,11 @@ def get_page(url, page_type, cookies):
                 if test_path:
                     first_page_item = parse_rank_first(response)
                     scroll_page = html.xpath('//div[@class="p13n-desktop-grid"]/@data-client-recs-list')[0]
-                    data = get_scroll_page_data(scroll_page)
+                    bs_category = "zg_bs_300189"
+                    data = get_scroll_page_data(len(first_page_item), scroll_page, bs_category)
                     token = get_token(response)
-                    scroll_page_item = get_scroll_page_item(data,cookies,token)
-                    first_page_item += scroll_page_item
+                    scroll_page_item = get_scroll_page_item(data, cookies, token, url)
+                    # first_page_item += scroll_page_item
                     next_page_url = get_next_page_url(html)
                     return first_page_item, next_page_url
                 else:
@@ -205,15 +254,15 @@ def get_page(url, page_type, cookies):
             else:
                 logging.log("未知的错误" + response.status_code + response.text)
 
-page_first = "https://www.amazon.com/Best-Sellers-Electronics-Computer-Routers/zgbs/electronics/300189"
 
+page_first = "https://www.amazon.com/Best-Sellers-Computers-Accessories-Whole-Home-Mesh-Wi-Fi-Systems/zgbs/pc/17935294011/ref=zg_bs_pg_1?_encoding=UTF8&pg=1"
 
-frist_page_data,next_page_url  = get_page(page_first, "FIRST_PAGE_REQUEST", {})
+frist_page_data, next_page_url = get_page(page_first, "FIRST_PAGE_REQUEST", {})
 
-print(frist_page_data)
-print(next_page_url)
+# print(frist_page_data)
+# print(next_page_url)
 
 if len(next_page_url):
-    next_page_data,next_page_url = get_page(next_page_url, "OTHER_PAGE_REQUEST_FIRST",{})
-    print(next_page_data)
-    print(next_page_url)
+    next_page_data, next_page_url = get_page(next_page_url, "OTHER_PAGE_REQUEST_FIRST", {})
+    # print(next_page_data)
+    # print(next_page_url)
