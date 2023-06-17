512268294093075a21537debeeae5a37fafc3d97
Administrator <393089025@qq.com>
2023-06-17 18:08:31 +0800

    amazon项目建表

--- 项目目录/亚马逊/address_change.py
+++ 项目目录/亚马逊/address_change.py
@@ -32,46 +32,32 @@ def get_cookie(response, cookie):
 
 def change_address(cookies,zip_code,glow_token):
     headers = {
-        "authority": "fls-na.amazon.com",
-        "accept": "*/*",
-        "accept-language": "en-US,en;q=0.9",
-        "anti-csrftoken-a2z": glow_token,
-        "cache-control": "no-cache",
-        "device-memory": "8",
-        "downlink": "0.65",
-        "dpr": "0.8",
-        "ect": "4g",
-        "pragma": "no-cache",
-        "referer": "https://www.amazon.com/",
-        "rtt": "250",
-        "sec-ch-device-memory": "8",
-        "sec-ch-dpr": "0.8",
-        "sec-ch-ua": "^\\^Not.A/Brand^^;v=^\\^8^^, ^\\^Chromium^^;v=^\\^114^^, ^\\^Google",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "^\\^Windows^^",
-        "sec-ch-ua-platform-version": "^\\^15.0.0^^",
-        "sec-ch-viewport-width": "387",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "no-cors",
-        "sec-fetch-site": "same-site",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        "viewport-width": "387",
-        "x-requested-with": "XMLHttpRequest",
-        "origin": "https://www.amazon.com",
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Content-Type": "text/plain;charset=UTF-8",
-        "Origin": "https://www.amazon.com",
-        "Pragma": "no-cache",
-        "Referer": "https://www.amazon.com/",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "no-cors",
-        "Sec-Fetch-Site": "same-site",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        "content-type": "text/plain;charset=UTF-8"
-    }
+    "Accept": "text/html,*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Anti-Csrftoken-A2z": glow_token,
+    "Cache-Control": "no-cache",
+    "Device-Memory": "8",
+    "Downlink": "1.3",
+    "Dpr": "0.8",
+    "Ect": "3g",
+    "Pragma": "no-cache",
+    "Referer": "https://www.amazon.com/gp/bestsellers/pc/17935294011/ref=pd_zg_hrsr_pc",
+    "Rtt": "250",
+    "Sec-Ch-Device-Memory": "8",
+    "Sec-Ch-Dpr": "0.8",
+    "Sec-Ch-Ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Ch-Ua-Platform-Version": "\"15.0.0\"",
+    "Sec-Ch-Viewport-Width": "801",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Viewport-Width": "801",
+    "X-Requested-With": "XMLHttpRequest"
+}
     url = "https://www.amazon.com/portal-migration/hz/glow/get-rendered-address-selections"
     params = {
         "deviceType": "desktop",
@@ -79,39 +65,39 @@ def change_address(cookies,zip_code,glow_token):
         "storeContext": "pc",
         "actionSource": "desktop-modal"
     }
-    response = requests.post(url, headers=headers, cookies=cookies, params=params)
+    response = requests.post(url=url, headers=headers, cookies=cookies, params=params)
     print(response.status_code)
     print(response.text)
     a2z = str(re.findall('CSRF_TOKEN: "(.*?)",', response.text)[0])
     headers = {
-        "authority": "www.amazon.com",
-        "accept": "text/html,*/*",
-        "accept-language": "en-US,en;q=0.9",
-        "anti-csrftoken-a2z": a2z,
-        "cache-control": "no-cache",
-        "content-type": "application/json",
-        "device-memory": "8",
-        "downlink": "1.45",
-        "dpr": "0.8",
-        "ect": "3g",
-        "origin": "https://www.amazon.com",
-        "pragma": "no-cache",
-        "referer": "https://www.amazon.com/gp/bestsellers/pc/17935294011/ref=pd_zg_hrsr_pc",
-        "rtt": "400",
-        "sec-ch-device-memory": "8",
-        "sec-ch-dpr": "0.8",
-        "sec-ch-ua": "^\\^Not.A/Brand^^;v=^\\^8^^, ^\\^Chromium^^;v=^\\^114^^, ^\\^Google",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "^\\^Windows^^",
-        "sec-ch-ua-platform-version": "^\\^15.0.0^^",
-        "sec-ch-viewport-width": "1267",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        "viewport-width": "1267",
-        "x-requested-with": "XMLHttpRequest"
-    }
+    "Accept": "text/html,*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Anti-Csrftoken-A2z": a2z,
+    "Cache-Control": "no-cache",
+    "Content-Type": "application/json",
+    "Device-Memory": "8",
+    "Downlink": "1.45",
+    "Dpr": "0.8",
+    "Ect": "3g",
+    "Origin": "https://www.amazon.com",
+    "Pragma": "no-cache",
+    "Referer": "https://www.amazon.com/gp/bestsellers/pc/17935294011/ref=pd_zg_hrsr_pc",
+    "Rtt": "300",
+    "Sec-Ch-Device-Memory": "8",
+    "Sec-Ch-Dpr": "0.8",
+    "Sec-Ch-Ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Ch-Ua-Platform-Version": "\"15.0.0\"",
+    "Sec-Ch-Viewport-Width": "801",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Viewport-Width": "801",
+    "X-Requested-With": "XMLHttpRequest"
+}
     url = "https://www.amazon.com/portal-migration/hz/glow/address-change"
     params = {
         "actionSource": "glow"
@@ -129,26 +115,7 @@ def change_address(cookies,zip_code,glow_token):
     print(response.text)
     print(response)
 
-url = "https://www.amazon.com/gp/bestsellers/pc/17935294011/ref=pd_zg_hrsr_pc"
 
-headers_first = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache",
-    "Sec-Ch-Ua": "\"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"",
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "\"Windows\"",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
-}
-
-cookies = {}
 
 
 
@@ -163,15 +130,51 @@ for i in range(3):
         '//div[@class="zg-grid-general-faceout"]//span[@class="_cDEzb_p13n-sc-price_3mJ9Z"]/text()')
         cookies = get_cookie(response, cookies)
         if test_path:
-            glow_token = html.xpath("//input[@id='glowValidationToken']/@value")
+            glow_token = str(html.xpath("//input[@id='glowValidationToken']/@value")[0])
 
             print(glow_token)
 
-            change_address(cookies, "10014", glow_token[0])
+            change_address(cookies, "10014", glow_token)
 
             response = requests.get(url, headers=headers_first, cookies=cookies)
         else:
             continue
 
-
-
+if __name__ == '__main__':
+    cookies = {}
+    for i in range(5):
+        url = "https://www.amazon.com/gp/bestsellers/pc/17935294011/ref=pd_zg_hrsr_pc"
+        headers_first = {
+            "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "sec-ch-ua": "^\\^Not.A/Brand^^;v=^\\^8^^, ^\\^Chromium^^;v=^\\^114^^, ^\\^Google",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "^\\^Windows^^",
+            "sec-fetch-dest": "image",
+            "sec-fetch-mode": "no-cors",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "device-memory": "8",
+            "downlink": "1.45",
+            "dpr": "0.8",
+            "ect": "3g",
+            "referer": "https://www.amazon.com/gp/bestsellers/pc/17935294011/ref=pd_zg_hrsr_pc",
+            "rtt": "300",
+            "sec-ch-device-memory": "8",
+            "sec-ch-dpr": "0.8",
+            "sec-ch-ua-platform-version": "^\\^15.0.0^^",
+            "sec-ch-viewport-width": "801",
+            "viewport-width": "801"
+        }
+        response = requests.get(url, headers=headers_first, cookies=cookies)
+        if len(response.cookies.keys()) > 0:
+            for key in response.cookies.keys():
+                cookies[key] = response.cookies.get(key)
+        if cookies.get("session-id") and cookies.get("session-id-time") and cookies.get("i18n-prefs") and cookies.get("ubid-main") and cookies.get("session-token") and cookies.get("csm-hit"):
+            break
+        else:
+            continue
